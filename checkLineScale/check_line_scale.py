104a665afd0feae0e1d41530287204b0d3718c4c
구민구 <koomk97@naver.com>
2021-02-18 20:39:50 +0900

    add line scale

--- checkLineScale/check_line_scale.py
+++ checkLineScale/check_line_scale.py
@@ -5,16 +5,31 @@
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-image = cv2.imread('./test-photo/test1.png')
+dirpath = "./test-photo/"
+imgname = "test1"
+imgname = "page-1-crop"
+
+image = cv2.imread(dirpath+ imgname+'.png')
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+block_size = 9
+c = 0
+thresh = cv2.adaptiveThreshold(
+                        np.invert(image_gray), 
+                        255, #흑백 명도 최대값
+                        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,  #평균 계산 방법 / ADAPTIVE_THRESH_MEAN_C
+                        cv2.THRESH_BINARY, # 흑백 반전 여부 /THRESH_BINARY_INV
+                        block_size, # 블록 사이즈 
+                        c # 가감 상수
+                        )
 
 contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 print(contours)
 image = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
 
-cv2.imshow("image",cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+cv2.imshow("line scale image", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 cv2.waitKey(0)
 
 
