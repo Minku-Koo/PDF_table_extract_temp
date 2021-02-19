443f219fb6209cf833987ed1aa8c3d3543a88e7f
구민구 <koomk97@naver.com>
2021-02-19 19:03:04 +0900

    check line scale

--- checkLineScale/check_line_scale.py
+++ checkLineScale/check_line_scale.py
@@ -6,6 +6,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 dirpath = "./test-photo/"
 imgname = "test1"
@@ -16,7 +17,11 @@ image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 block_size = 9
 c = 0
-thresh = cv2.adaptiveThreshold(
+
+region = [90, 20, 40, 50]
+line_scale = 0
+for i in range(1):
+    thresh = cv2.adaptiveThreshold(
                         np.invert(image_gray), 
                         255, #흑백 명도 최대값
                         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,  #평균 계산 방법 / ADAPTIVE_THRESH_MEAN_C
@@ -24,30 +29,67 @@ thresh = cv2.adaptiveThreshold(
                         block_size, # 블록 사이즈 
                         c # 가감 상수
                         )
+    
+    print("threshdhold size:",thresh.shape)
+    line_scale += 20
+    # if vertical 
+    # size = thresh.shape[0] // line_scale
+    # el = cv2.getStructuringElement(cv2.MORPH_RECT, (1, size))
+    # name = "v"+str(line_scale)
+    # if horizontal
+    size = thresh.shape[1] // line_scale
+    el = cv2.getStructuringElement(cv2.MORPH_RECT, ( size, 1))
+    name="h"+str(line_scale)
 
-size = 10
-itr = 0
-el = cv2.getStructuringElement(cv2.MORPH_RECT, (1, size))
-thresh = cv2.erode(thresh, el, iterations=itr)
-thresh = cv2.dilate(thresh, el) #오프닝
+    region_mask = np.zeros(thresh.shape)
+    x, y, w, h = region
+    region_mask[y : y + h, x : x + w] = 1
+    print( "1 count:",np.count_nonzero(thresh>0) )
+    thresh = np.multiply(thresh, region_mask)
+    
+    print( "1 count:",np.count_nonzero(thresh>0) )
 
-contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-print(contours)
-# image = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-threshd = cv2.drawContours(thresh, contours, -1, (0, 255, 0), 3)
-
-lines = []
-liness = []
-for c in contours:
-    x, y, w, h = cv2.boundingRect(c)
-    x1, x2 = x, x + w
-    y1, y2 = y, y + h
-    lines.append(((x1 + x2) // 2, y2, (x1 + x2) // 2, y1))
-    liness.append((x1, (y1 + y2) // 2, x2, (y1 + y2) // 2))
-print(lines)
-print(liness)
-# cv2.imshow("line scale image", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-cv2.imshow("line scale image", cv2.cvtColor(threshd, cv2.COLOR_BGR2RGB))
-cv2.waitKey(0)
+    thresh = cv2.erode(thresh, el)
+    thresh = cv2.dilate(thresh, el) #오프닝
+    
+    try:
+        _, contours, _ = cv2.findContours(
+            thresh.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+    except ValueError:
+        # for opencv backward compatibility
+        contours, _ = cv2.findContours(
+            thresh.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+    print(contours)
+    
+    image = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    # threshd = cv2.drawContours(thresh, contours, -1, (0, 255, 0), 3)
+    '''
+    lines = []
+    liness = []
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        x1, x2 = x, x + w
+        y1, y2 = y, y + h
+        lines.append(((x1 + x2) // 2, y2, (x1 + x2) // 2, y1))
+        liness.append((x1, (y1 + y2) // 2, x2, (y1 + y2) // 2))
+    '''
+    # print(lines)
+    # print(liness)
+    # cv2.imshow("line scale image", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # j = input()
+    
+    t_mark = thresh[ y:y+h, x:x+w]
+    np.set_printoptions(threshold=sys.maxsize)
+    
+    print(t_mark)
+    
+    cv2.imshow("line scale image", thresh)# cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB))
+    # img = cv2.hconcat([cv2.cvtColor(image, cv2.COLOR_BGR2RGB), cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB)])
+    # cv2.imshow("line scale image "+str(line_scale), cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    print(">>>",line_scale)
+    # cv2.imwrite("./"+name+".png", thresh)
+    cv2.waitKey(0)
 
 
